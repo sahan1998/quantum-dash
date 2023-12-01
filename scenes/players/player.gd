eca60828b8af3e6435d471ba3d6b42b2cf5b4aae
sahan1998 <72294946+sahan1998@users.noreply.github.com>
2023-12-01 22:38:20 +0300

    portal jump in animation

--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -17,7 +17,11 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimationPlayer.play("jump")
-
+	
+	# Handle Dimension Swap
+	if Input.is_action_just_pressed("primary action") and is_on_floor():
+		$AnimationPlayer.play("portal_jump_in")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 #	var direction = Input.get_axis("ui_left", "ui_right")
